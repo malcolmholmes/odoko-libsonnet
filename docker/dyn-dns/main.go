53c6a8eae9ed98fc052b930458d3740df2dfc625
Malcolm Holmes <mdh@odoko.co.uk>
2020-10-30 21:36:02 +0000

    dyndns finished and working

--- docker/dyn-dns/main.go
+++ docker/dyn-dns/main.go
@@ -5,9 +5,10 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
-	"net"
 	"net/http"
 	"os"
+	"sort"
+	"strings"
 	"time"
 
 	"github.com/prometheus/client_golang/prometheus"
@@ -28,83 +29,42 @@ var (
 		Name: "dyndns_domain_count",
 		Help: "The number of tracked domains",
 	})
-	domainsChecked = promauto.NewCounter(prometheus.CounterOpts{
-		Name: "dyndns_domains_checked_total",
-		Help: "The total number of domains checked",
-	})
-	domainsUpdated = promauto.NewCounter(prometheus.CounterOpts{
-		Name: "dyndns_domains_updated_total",
-		Help: "The total number of domains updated",
-	})
+	domainsChecked = promauto.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "dyndns_domains_checked_total",
+			Help: "The total number of domains checked",
+		},
+		[]string{"domain"},
+	)
+	domainsUpdated = promauto.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "dyndns_domains_updated_total",
+			Help: "The total number of domains updated",
+		},
+		[]string{"domain"},
+	)
+	errorsOccurred = promauto.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "dyndns_error_total",
+			Help: "The total number of errors",
+		},
+		[]string{"type", "domain"},
+	)
+	apiCalls = promauto.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "dyndns_google_api_call_total",
+			Help: "The total number of calls to the Google API",
+		},
+		[]string{"type", "domain"},
+	)
+	apiCallErrors = promauto.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "dyndns_google_api_call_error_total",
+			Help: "The total number of failed calls to the Google API",
+		},
+		[]string{"type", "domain"},
+	)
 )
-
-func main() {
-
-	googleProject := os.Getenv("GOOGLE_PROJECT")
-	clientset := getK8sClientSet()
-	ctx := context.Background()
-	dnsService, err := dns.NewService(ctx)
-
-	go func() {
-		http.Handle("/metrics", promhttp.Handler())
-		http.ListenAndServe(":8080", nil)
-	}()
-
-	if err != nil {
-		panic(err.Error())
-	}
-	for {
-		_, err := loop(googleProject, clientset, dnsService)
-		if err != nil {
-			log.Println("Error:", err.Error())
-		}
-		log.Println("Checks completed")
-		time.Sleep(60 * time.Second)
-	}
-}
-
-func loop(googleProject string, clientset *kubernetes.Clientset, dnsService *dns.Service) (bool, error) {
-	domains, err := getDomains(*clientset)
-	if err != nil {
-		return false, err
-	}
-	myIP, err := lookupMyIP()
-	if err != nil {
-		return false, err
-	}
-	changesMade := false
-	for _, domain := range domains {
-		ip, err := lookupIPViaGoogle(googleProject, dnsService, domain)
-		if err != nil {
-			return false, err
-		}
-		domain.IP = ip
-		log.Printf("Checking %s: %s <-> %s", domain.Name, domain.IP, myIP)
-		domainsChecked.Inc()
-		if domain.IP != myIP {
-			err = updateDNS(googleProject, dnsService, domain, myIP)
-			if err != nil {
-				return false, err
-			}
-			domainsUpdated.Inc()
-			changesMade = true
-		}
-	}
-	return changesMade, nil
-}
-
-func getK8sClientSet() *kubernetes.Clientset {
-
-	config, err := rest.InClusterConfig()
-	if err != nil {
-		panic(err.Error())
-	}
-	clientset, err := kubernetes.NewForConfig(config)
-	if err != nil {
-		panic(err.Error())
-	}
-	return clientset
-}
 
 // Domain describes everything required to update a domain
 type Domain struct {
@@ -113,15 +73,151 @@ type Domain struct {
 	IP   string
 }
 
-func getDomains(clientset kubernetes.Clientset) ([]Domain, error) {
-	ingresses, err := clientset.ExtensionsV1beta1().Ingresses("").List(metav1.ListOptions{})
+func ifErrorPanic(err error) {
 	if err != nil {
 		panic(err.Error())
 	}
-	log.Printf("%d ingresses found in the cluster", len(ingresses.Items))
+}
+
+type serviceSet struct {
+	k8s           *kubernetes.Clientset
+	dns           *dns.Service
+	googleProject string
+}
+
+func getServiceSet() serviceSet {
+	serviceSet := serviceSet{}
+
+	config, err := rest.InClusterConfig()
+	ifErrorPanic(err)
+	clientset, err := kubernetes.NewForConfig(config)
+	ifErrorPanic(err)
+	serviceSet.k8s = clientset
+
+	ctx := context.Background()
+	dnsService, err := dns.NewService(ctx)
+	ifErrorPanic(err)
+	serviceSet.dns = dnsService
+
+	serviceSet.googleProject = os.Getenv("GOOGLE_PROJECT")
+	return serviceSet
+
+}
+
+type domainSet map[string]Domain
+
+func main() {
+
+	services := getServiceSet()
+
+	go func() {
+		http.Handle("/metrics", promhttp.Handler())
+		http.ListenAndServe(":8080", nil)
+	}()
+
+	myIP, err := lookupMyIP()
+	ifErrorPanic(err)
+
+	domains, err := getDomains(services)
+	ifErrorPanic(err)
+	newIP, err := updateDomains(services, domains, myIP)
+	ifErrorPanic(err)
+	currentIP := newIP
+
+	for {
+		myIP, err = lookupMyIP()
+		if err != nil {
+			log.Println("Error:", err.Error())
+			continue
+		}
+		newDomains, err := getDomains(services)
+		if err != nil {
+			log.Println("Error:", err.Error())
+			continue
+		}
+		if domainsChanged(domains, newDomains) || myIP != currentIP {
+			if myIP != currentIP {
+				log.Printf("IP changed from %s to %s", currentIP, myIP)
+			} else {
+				log.Printf("Domain change detected, %d before, now %d", len(domains), len(newDomains))
+			}
+			domains = newDomains
+			newIP, err = updateDomains(services, domains, myIP)
+			if err != nil {
+				log.Println("Error:", err.Error())
+				continue
+			}
+			currentIP = newIP
+		}
+		time.Sleep(5 * time.Second)
+	}
+}
+
+func lookupMyIP() (string, error) {
+	resp, err := http.Get("http://ipecho.net/plain")
+	if err != nil {
+		errorsOccurred.With(prometheus.Labels{"type": "lookupMyIP:curl", "domain": ""}).Inc()
+		return "", err
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		errorsOccurred.With(prometheus.Labels{"type": "lookupMyIP:ReadAll", "domain": ""}).Inc()
+		return "", err
+	}
+	return string(body), err
+}
+
+func domainsChanged(oldDomains, newDomains domainSet) bool {
+	oldKeys := make([]string, len(oldDomains))
+	for k := range oldDomains {
+		oldKeys = append(oldKeys, k)
+	}
+	sort.Strings(oldKeys)
+	old := strings.Join(oldKeys, "|")
+	newKeys := make([]string, len(newDomains))
+	for k := range newDomains {
+		newKeys = append(newKeys, k)
+	}
+	sort.Strings(newKeys)
+	new := strings.Join(newKeys, "|")
+
+	return old != new
+}
+
+func lookupIPViaGoogle(services serviceSet, zone, domain string) (string, error) {
+	recordSets, err := services.dns.ResourceRecordSets.List(services.googleProject, zone).Do()
+	apiCalls.With(prometheus.Labels{"type": "list", "domain": domain}).Inc()
+	if err != nil {
+		apiCallErrors.With(prometheus.Labels{"type": "list", "domain": domain}).Inc()
+		errorsOccurred.With(prometheus.Labels{"type": "lookupIPViaGoogle:dns-list", "domain": domain}).Inc()
+		return "", err
+	}
+	dottedDomain := domain + "."
+	for _, recordSet := range recordSets.Rrsets {
+		if recordSet.Name == dottedDomain && recordSet.Type == "A" {
+			if len(recordSet.Rrdatas) == 0 {
+				errorsOccurred.With(prometheus.Labels{"type": "no-ip-found", "domain": domain}).Inc()
+				return "", fmt.Errorf("Domain %s did not return an IP", domain)
+			} else if len(recordSet.Rrdatas) > 1 {
+				errorsOccurred.With(prometheus.Labels{"type": "multiple-ips-found", "domain": domain}).Inc()
+				return "", fmt.Errorf("Domain %s multiple IPs. This is not supported", domain)
+			}
+			return recordSet.Rrdatas[0], nil
+		}
+	}
+	return "x.x.x.x", nil
+}
+
+func getDomains(services serviceSet) (domainSet, error) {
+	ingresses, err := services.k8s.ExtensionsV1beta1().Ingresses("").List(metav1.ListOptions{})
+	if err != nil {
+		errorsOccurred.With(prometheus.Labels{"type": "k8s-list-ingresses", "domain": ""}).Inc()
+		return nil, err
+	}
 	ingressCount.Set(float64(len(ingresses.Items)))
 
-	domains := []Domain{}
+	domains := domainSet{}
 
 	for _, ingress := range ingresses.Items {
 		var zone string
@@ -136,59 +232,34 @@ func getDomains(clientset kubernetes.Clientset) ([]Domain, error) {
 		}
 
 		for _, rule := range ingress.Spec.Rules {
-			domains = append(domains, Domain{zone, rule.Host, ""})
+			ip, err := lookupIPViaGoogle(services, zone, rule.Host)
+			if err != nil {
+				return nil, err
+			}
+			domains[rule.Host] = Domain{zone, rule.Host, ip}
 		}
 	}
-	log.Printf("%d domains with matching annotation odoko.com/dyn-dns-zone", len(domains))
 	domainCount.Set(float64(len(domains)))
 	return domains, nil
 }
 
-func lookupMyIP() (string, error) {
-	resp, err := http.Get("http://ipecho.net/plain")
-	if err != nil {
-		return "", err
-	}
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	return string(body), err
-}
+func updateDomains(services serviceSet, domains domainSet, myIP string) (string, error) {
 
-func lookupIPViaGoogle(googleProject string, dnsService *dns.Service, domain Domain) (string, error) {
-	recordSets, err := dnsService.ResourceRecordSets.List(googleProject, domain.Zone).Do()
-	if err != nil {
-		return "", err
-	}
-	dottedDomain := domain.Name + "."
-	for _, recordSet := range recordSets.Rrsets {
-		if recordSet.Name == dottedDomain {
-			if len(recordSet.Rrdatas) == 0 {
-				return "", fmt.Errorf("Domain %s did not return an IP", domain.Name)
-			} else if len(recordSet.Rrdatas) > 1 {
-				return "", fmt.Errorf("Domain %s multiple IPs. This is not supported", domain.Name)
+	for _, domain := range domains {
+		log.Printf("Checking %s: %s <-> %s", domain.Name, domain.IP, myIP)
+		domainsChecked.With(prometheus.Labels{"domain": domain.Name}).Inc()
+		if domain.IP != myIP {
+			err := updateDNS(services, domain, myIP)
+			if err != nil {
+				return "", err
 			}
-			return recordSet.Rrdatas[0], nil
+			domainsUpdated.With(prometheus.Labels{"domain": domain.Name}).Inc()
 		}
 	}
-	return "", fmt.Errorf("Domain %s not found", domain.Name)
+	return myIP, nil
 }
 
-func lookupIPViaDNS(domain Domain) (string, error) {
-	ips, err := net.LookupIP(domain.Name)
-	if err != nil {
-		return "", err
-	}
-	if len(ips) == 0 {
-		return "", fmt.Errorf("%s did not resolve to an IP", domain.Name)
-	} else if len(ips) > 1 {
-		return "", fmt.Errorf("%s resolves to multiple IPs. This is not supported", domain.Name)
-	}
-	ip := ips[0]
-
-	return ip.String(), nil
-}
-
-func updateDNS(googleProject string, dnsService *dns.Service, domain Domain, myIP string) error {
+func updateDNS(services serviceSet, domain Domain, myIP string) error {
 	log.Printf("Updating %s", domain.Name)
 
 	change := &dns.Change{}
@@ -206,12 +277,18 @@ func updateDNS(googleProject string, dnsService *dns.Service, domain Domain, myI
 	}
 
 	change.Additions = append(change.Additions, &addition)
-	change.Deletions = append(change.Deletions, &deletion)
+	if domain.IP != "x.x.x.x" {
+		change.Deletions = append(change.Deletions, &deletion)
+	}
 
-	_, err := dnsService.Changes.Create(googleProject, domain.Zone, change).Do()
+	_, err := services.dns.Changes.Create(services.googleProject, domain.Zone, change).Do()
+	apiCalls.With(prometheus.Labels{"type": "change", "domain": domain.Name}).Inc()
 	if err != nil {
+		apiCallErrors.With(prometheus.Labels{"type": "change", "domain": domain.Name}).Inc()
+		errorsOccurred.With(prometheus.Labels{"type": "updateDNS:dns-change", "domain": domain.Name}).Inc()
 		return err
 	}
+
 	log.Printf("Update complete for %s", domain.Name)
 
 	return nil
