0cb7ada9a98d9f7339b91b282bb4c1ebd9808a02
Malcolm Holmes <mdh@odoko.co.uk>
2020-12-26 14:47:29 +0000

    Upgrade wp, remove pod delete call

--- docker/wordpress/build.sh
+++ docker/wordpress/build.sh
@@ -11,9 +11,3 @@ docker build -t odoko/wordpress:$VERSION .
 if [[ " $* " =~ .*\ push\ .* ]]; then
   docker push odoko/wordpress:$VERSION
 fi
-
-if [[ " $* " =~ .*\ deploy\ .* ]]; then
-  echo deleting pod
-  kubectl --context $CONTEXT delete pod -n $NS wordpress-0
-
-fi
