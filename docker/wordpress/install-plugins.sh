fcfdc873a9912c47c545474585a893c64cc9fdc9
Malcolm Holmes <mdh@odoko.co.uk>
2021-04-17 21:03:15 +0100

    Chown, less verbose, tidy

--- docker/wordpress/install-plugins.sh
+++ docker/wordpress/install-plugins.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 PLUGINS_FILE=${PLUGINS_FILE?"PLUGINS_FILE required"}
+DOWNLOADS_DIR=/var/www/html/wp-content/downloads
+PLUGIN_ROOT=/usr/src/wordpress/wp-content/plugins
 
-cd /wordpress
-
-mkdir -p /usr/src/wordpress/wp-content/plugins
-mkdir -p /wordpress/downloaded-plugins
+mkdir -p $DOWNLOADS_DIR
+mkdir -p $PLUGIN_ROOT
 for PLUGIN_URL in $(jq -r '.plugins[].url' $PLUGINS_FILE); do
   echo "Installing plugin $PLUGIN_URL..."
-  ZIP_FILE=/wordpress/downloaded-plugins/$(basename $PLUGIN_URL)
+  ZIP_FILE=$DOWNLOADS_DIR/$(basename $PLUGIN_URL)
+  echo "Checking for $ZIP_FILE.."
   if [ -e $ZIP_FILE ]; then
     echo "  $ZIP_FILE exists - skipping"
     continue
@@ -20,14 +21,16 @@ for PLUGIN_URL in $(jq -r '.plugins[].url' $PLUGINS_FILE); do
     echo "  Unknown protocol for $PLUGIN_URL"
     exit
   fi
-  unzip -o -d /usr/src/wordpress/wp-content/plugins $ZIP_FILE
+  unzip -o -q -d /usr/src/wordpress/wp-content/plugins $ZIP_FILE
 done
+echo "Change ownership and clone plugins"
+chown -R www-data:www-data /usr/src/wordpress/wp-content/plugins
 
+echo "Install themes"
 mkdir -p /usr/src/wordpress/wp-content/themes
-mkdir -p /wordpress/downloaded-themes
 for THEME_URL in $(jq -r '.themes[].url' $PLUGINS_FILE); do
   echo "Installing theme $THEME_URL..."
-  ZIP_FILE=/wordpress/downloaded-themes/$(basename $THEME_URL)
+  ZIP_FILE=/$DOWNLOADS_DIR/$(basename $THEME_URL)
   echo "Looking for $ZIP_FILE"
   if [ -e $ZIP_FILE ]; then
     echo "  $ZIP_FILE exists - skipping"
@@ -40,7 +43,8 @@ for THEME_URL in $(jq -r '.themes[].url' $PLUGINS_FILE); do
     echo "Unknown protocol for $THEME_URL"
     exit
   fi
-  unzip -o -d /usr/src/wordpress/wp-content/themes/ $ZIP_FILE
+  unzip -o -q -d /usr/src/wordpress/wp-content/themes/ $ZIP_FILE
 done
+chown -R www-data:www-data /usr/src/wordpress/wp-content/themes
 
 echo "Done."
